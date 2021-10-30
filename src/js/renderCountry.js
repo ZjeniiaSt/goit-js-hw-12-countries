6d3f3f6e57749fc26a5e2f15cfa2bf2f9f9b6903
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-10-30 18:40:12 +0300

    1

--- src/js/renderCountry.js
+++ src/js/renderCountry.js
@@ -2,10 +2,22 @@ import API from './api-service';
 import getRefs from './getRefs';
 import countryMarkup from '../templates/counrty_markup.hbs';
 import listMarkup from '../templates/list_markup.hbs';
+import debounce from 'lodash.debounce';
+import '@pnotify/core/dist/BrightTheme.css';
+import { alert, Stack } from '@pnotify/core';
+
+const myStack = new Stack({
+  dir1: 'up',
+});
+
+alert({
+  text: "I'm a notice centered at the bottom!",
+  stack: myStack,
+});
 
 const refs = getRefs();
 
-refs.searchForm.addEventListener('input', onSearh);
+refs.searchForm.addEventListener('input', debounce(onSearh, 500));
 
 function onSearh(e) {
   e.preventDefault();
@@ -16,12 +28,20 @@ function onSearh(e) {
 
 function renderCountry(country) {
   let countryList = country.length;
+
   if (countryList === 1) {
     refs.cardContainer.innerHTML = countryMarkup(country);
   } else if (countryList <= 10) {
     refs.cardContainer.innerHTML = listMarkup(country);
   } else if (countryList > 10) {
     console.log('дописати');
+    const myStack = new Stack({
+      dir1: 'down',
+    });
+    alert({
+      text: 'Give me more letters and I will find the country',
+      stack: myStack,
+    });
   }
 }
 
